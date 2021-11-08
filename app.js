cdb81b8a12b99ef2e81a07ad40a022b2d8bf28dc
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-11-08 20:55:06 +0700

    added special attack method

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,9 +17,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       //   const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
@@ -28,6 +33,12 @@ const app = Vue.createApp({
       //   const attackValue = Math.floor(Math.random() * (15 - 8)) + 8;
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
